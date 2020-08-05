7a9e65efefb5181d18e7281007641e72f2b6447a
Arjen Karel <arjen@marketingtracer.com>
2020-08-05 13:19:11 +0200

    Updated module
    Some desc

--- src/fasticon.js
+++ src/fasticon.js
@@ -1,23 +1,28 @@
-const fnFalink = () => {
-    let link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = '/include/scss/fontawesome/font-awesome.min.css';
-    document.head.appendChild(link);
+const asyncIconFont = ()=>{
+
+    const fnIconIconFontDomLink = () => {
+        let link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = '/include/scss/fontawesome/font-awesome.min.css';
+        document.head.appendChild(link);
+    }
+
+
+    const fnIconFontObserver = () => {
+        let fas = document.querySelectorAll('.fa');
+        let observer = new IntersectionObserver((entry, observer) => {
+            if (entry[0].intersectionRatio > 0) {
+                fnIconIconFontDomLink();
+                observer.disconnect();
+            }
+        });
+        fas.forEach(fa => {
+            observer.observe(fa);
+        });
+    }
+
+    (('IntersectionObserver' in window) ? fnFaObserver : fnIconIconFontDomLink)();
+
 }
 
-
-const fnFaObserver = () => {
-    let fas = document.querySelectorAll('.fa');
-    let observer = new IntersectionObserver((entry, observer) => {
-        if (entry[0].intersectionRatio > 0) {
-            fnFalink();
-            observer.disconnect();
-        }
-    });
-    fas.forEach(fa => {
-        observer.observe(fa);
-    });
-}
-
-(('IntersectionObserver' in window) ? fnFaObserver : fnFalink)();
